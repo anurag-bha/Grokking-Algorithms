da7e60abf3078131bac651e92cb42faf80a005b4
anurag-bha <bhttchr6@gmail.com>
2024-08-21 16:35:42 -0700

    Add files via upload

---
+++ unittesting/test_class.py
@@ -0,0 +1,5 @@
+from Grokking_Algorithms import GrokkingAlgorithms
+class TestClass:
+        def test_answer(self):        
+                assert GrokkingAlgorithms().BinarySearch([1, 4, 5, 7, 9, 10], 5) == 2
+        
