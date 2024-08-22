3107f058b179fc7ecd3425d4a68afaef37017d7f
anurag-bha <bhttchr6@gmail.com>
2024-08-21 23:00:13 -0700

    Update test_class.py with comment on import of files

--- unittesting/test_class.py
+++ unittesting/test_class.py
@@ -1,5 +1,7 @@
 import sys
 path = sys.path.append(".") # Adds higher directory to python modules path.
+# https://stackoverflow.com/questions/30669474/beyond-top-level-package-error-in-relative-import
+# Check the above stackoverflow for explaination of the single dot for accessing higher-level files
 from Grokking_Algorithms import GrokkingAlgorithms
 class TestClass:
         def test_answer(self):        
