e35f2e14af97acffc8e6e3fd7095feafa49a18ab
anurag-bha <bhttchr6@gmail.com>
2024-08-21 22:24:32 -0700

    Update test_class.py with link to base class

--- unittesting/test_class.py
+++ unittesting/test_class.py
@@ -1,4 +1,4 @@
-from Grokking_Algorithms import GrokkingAlgorithms
+from ..Grokking_Algorithms import GrokkingAlgorithms
 class TestClass:
         def test_answer(self):        
                 assert GrokkingAlgorithms().BinarySearch([1, 4, 5, 7, 9, 10], 5) == 2
