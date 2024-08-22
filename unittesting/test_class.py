27d97b44e2ee1bf88ead8ba508be218ef1263e97
anurag-bha <bhttchr6@gmail.com>
2024-08-21 22:54:31 -0700

    Update test_class.py

--- unittesting/test_class.py
+++ unittesting/test_class.py
@@ -1,4 +1,6 @@
-from ..Grokking_Algorithms import GrokkingAlgorithms
+import sys
+path = sys.path.append(".") # Adds higher directory to python modules path.
+from Grokking_Algorithms import GrokkingAlgorithms
 class TestClass:
         def test_answer(self):        
                 assert GrokkingAlgorithms().BinarySearch([1, 4, 5, 7, 9, 10], 5) == 2
