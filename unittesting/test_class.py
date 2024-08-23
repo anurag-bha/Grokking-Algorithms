8f1628741c8eaf1124e998504bf94d03e1b4c8f5
anurag-bha <bhttchr6@gmail.com>
2024-08-22 23:45:56 -0700

    Update test_class.py

--- unittesting/test_class.py
+++ unittesting/test_class.py
@@ -1,5 +1,12 @@
+from os.path import dirname, abspath, join
 import sys
-path = sys.path.append(".") # Adds higher directory to python modules path.
+
+# Find code directory relative to our directory
+THIS_DIR = dirname(__file__)
+CODE_DIR = abspath(join(THIS_DIR, '..', 'src'))
+sys.path.append(CODE_DIR)
+#import sys
+#path = sys.path.append(".") # Adds higher directory to python modules path.
 # https://stackoverflow.com/questions/30669474/beyond-top-level-package-error-in-relative-import
 # Check the above stackoverflow for explaination of the single dot for accessing higher-level files
 from Grokking_Algorithms import GrokkingAlgorithms
