df0095d07873551ed6a2fb73ae3dc990b2a330bf
anurag-bha <bhttchr6@gmail.com>
2024-08-25 15:27:05 -0700

    Update Grokking_Algorithms.py

--- src/Grokking_Algorithms.py
+++ src/Grokking_Algorithms.py
@@ -3,6 +3,8 @@ class GrokkingAlgorithms:
 
         def __init__(self):
                 self.BinSearch_idx = []
+                self.MinNumber = []
+                self.MinId = []
         
         def BinarySearch(self, arr, keynumber):
                 low = 0
@@ -27,6 +29,28 @@ class GrokkingAlgorithms:
                         else:
                                 return None
                 return self.BinSearch_idx
+
+
+        def SmallestNumber(self, arr):
+                # function to find the smallest number in an array
+                self.MinNumber = arr[0]
+                self.MinId = 0
+                for i in range(len(arr)-1):
+                
+                    if arr[i+1] < self.MinNumber:
+                        self.MinNumber = arr[i+1]
+                        self.MinId = i+1
+                return self.MinNumber, self.MinId
+    
+    
+        def SelectionSort(self, arr):
+                # function for selection sort
+                new_arr = []
+                while len(arr)>0:
+                    self.SmallestNumber(arr)
+                    new_arr.append(self.MinNumber)
+                    arr.pop(self.MinId)
+                return new_arr
 
 
         
