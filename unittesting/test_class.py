efd39372b536b99a85ca1447a9c0edd6db106b25
anurag-bha <bhttchr6@gmail.com>
2024-08-25 15:25:53 -0700

    Update test_class.py

--- unittesting/test_class.py
+++ unittesting/test_class.py
@@ -3,4 +3,10 @@ from src import GrokkingAlgorithms
 class TestClass:
         def test_answer(self):        
                 assert GrokkingAlgorithms().BinarySearch([1, 4, 5, 7, 9, 10], 5) == 2
+
+        def test_selection_sort(self):
+                assert GrokkingAlgorithms().SelectionSort([1, 5, 4, 7, 10, 9]) == [1, 4, 5, 7, 9, 10]
+
+        def test_selection_sort_with_equal_numbers(self):
+                assert GrokkingAlgorithms().SelectionSort([1, 4, 4, 7, 10, 9, 5, 6]) == [1, 4, 4, 5, 6, 7, 9, 10]
         
